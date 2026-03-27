e49e2f32259bab9588195aa4a3d2c7723a301c50
kazakovmain <aleksei2705kazak@gmail.com>
2026-03-27 14:55:29 +0300

    add core

--- DjangoSNetwork/settings.py
+++ DjangoSNetwork/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core.apps.CoreConfig',
     'groups.apps.GroupsConfig',
     'posts.apps.PostsConfig',
     'django.contrib.admin',
